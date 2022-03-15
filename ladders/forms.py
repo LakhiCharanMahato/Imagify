b7dc960748168b0a1870d33f5cc1b63742f495c3
Lakhi Charan Mahato <lakhi.l.c.m@gmail.com>
2022-03-15 09:50:49 +0530

    added data type validation

--- ladders/forms.py
+++ ladders/forms.py
@@ -5,3 +5,12 @@ class LadderForm(forms.ModelForm):
     class Meta:
         model=Ladder
         fields=['title','description','file']
+
+    def clean_file(self):
+        cleaned_data=self.cleaned_data
+        file=cleaned_data.get('file')
+
+        if file.content_type not in ('video/mp4','image/jpeg','image/png'):
+            self.add_error("file","Only allowed data types are jpg, jpeg, png and mp4")
+        
+        return file
