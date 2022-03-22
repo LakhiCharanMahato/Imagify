aa0c725475fd0b7acd97764748f5d5bccb95c905
Lakhi Charan Mahato <lakhi.l.c.m@gmail.com>
2022-03-22 18:34:15 +0530

    added reset password feature

--- Imagify/settings.py
+++ Imagify/settings.py
@@ -135,3 +135,12 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT= BASE_DIR / 'static/media'
 MEDIA_URL= '/media/'
+
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- Imagify/urls.py
+++ Imagify/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
+from django.contrib.auth import views as auth_views
 from accounts.views import (
     login_view,
     logout_view,
@@ -39,7 +40,20 @@ urlpatterns = [
     path('logout/',logout_view),
     path('register/',register_view),
     path('upload/',upload_view),
-    path('gallery/',gallery_view)
+    path('gallery/',gallery_view),
+
+    path('reset_password/',
+        auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+        name="reset_password"),
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+        name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+        name="password_reset_confirm"),
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+        name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
