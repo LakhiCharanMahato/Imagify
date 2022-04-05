741386291173d996a89fe6cd5b5ecae807d46a36
Lakhi Charan Mahato <lakhi.l.c.m@gmail.com>
2022-04-05 16:05:04 +0530

    changed friends model

--- friends/admin.py
+++ friends/admin.py
@@ -1,26 +1,24 @@
 from django.contrib import admin
-from .models import FriendList, FriendRequest
+from .models import FriendList #, FriendRequest
 
 # Register your models here.
 class FriendListAdmin(admin.ModelAdmin):
-    list_filter=['user']
-    list_display=['user']
-    searh_fields=['user']
-    readonly_fields=['user']
+    list_filter=['user1']
+    list_display=['user1']
+    searh_fields=['user1']
+    readonly_fields=['user1','user2']
 
     class Meta:
         model=FriendList
 
 admin.site.register(FriendList, FriendListAdmin)
 
-class FriendRequestAdmin(admin.ModelAdmin):
-    list_filter=['sender','receiver']
-    list_display=['sender','receiver']
-    search_fields=['sender__username','sender__email','receiver__username','receiver__email']
+# class FriendRequestAdmin(admin.ModelAdmin):
+# list_filter=['sender','receiver']
+# list_display=['sender','receiver']
+# search_fields=['sender__username','sender__email','receiver__username','receiver__email']
 
-    class Meta:
-        model=FriendRequest
+# class Meta:
+# model=FriendRequest
 
-admin.site.register(FriendRequest,FriendRequestAdmin)
-
-
+# admin.site.register(FriendRequest,FriendRequestAdmin)
