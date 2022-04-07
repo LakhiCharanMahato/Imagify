ff06220df1e61795775b54fea8ad5d07633d335d
Lakhi Charan Mahato <lakhi.l.c.m@gmail.com>
2022-04-07 17:56:02 +0530

    Friend Request Already sent added

--- friends/views.py
+++ friends/views.py
@@ -8,25 +8,36 @@ def friends_finder_view(request):
     current_user=request.user
     # usersall=getattr(settings.AUTH_USER_MODEL,str)
 
-    # obj=User.objects.all().exclude(username=current_user)
-    obj1=User.objects.all().exclude(username=current_user).values_list('id',flat=True)
-    obj2=FriendList.objects.filter(user1=current_user).values_list('user2',flat=True)
-    diff=set(obj1)-set(obj2)
-    obj=[]
-    for i in diff:
-        obj.append(User.objects.get(id=i))
 
     query_dict=request.GET
     if request.method=='GET':
         query=query_dict.get('initialreceiver')
         print("LakhiOK",query,type(query))
         if query:
-            FriendList.send_friend_request(request.user,User.objects.get(id=int(query)))
+            user2=User.objects.get(id=int(query))
+            friendsalready=FriendList.objects.filter(user1=current_user).filter(user2=user2).exists()
+            if not friendsalready:    
+                FriendList.send_friend_request(request.user,user2)
 
+    # obj=User.objects.all().exclude(username=current_user)
+    obj1=User.objects.all().exclude(username=current_user).values_list('id',flat=True)
+    obj2=FriendList.objects.filter(user1=current_user).filter(is_active=False).values_list('user2',flat=True)
+    diff=set(obj1)-set(obj2)
+    obj=[]
+    for i in diff:
+        obj.append(User.objects.get(id=i))
 
     print(obj)
     context={
         'obj':obj,
-        'FriendList':FriendList
+        'FriendRequestList':FriendList.objects.filter(user1=current_user).filter(is_active=True).values_list('user2',flat=True)
     }
+
+
+            # context['FriendRequestList']=FriendList.objects.filter(user1=current_user).values_list('user2',flat=True)
+            # return render(request,'friends/finder.html',context)
+
+
+
+
     return render(request,'friends/finder.html',context)
